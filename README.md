ed23ceba45b04ef91a1443b691235cab2f68328b
pjan vandaele <pjan.vandaele@gmail.com>
2015-02-03 23:03:51 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## akka-ddd [![Build Status](https://travis-ci.org/pjan/akka-ddd.svg)](https://travis-ci.org/pjan/akka-ddd) [![Coverage Status](https://coveralls.io/repos/pjan/akka-ddd/badge.svg)](https://coveralls.io/r/pjan/akka-ddd)
+
+A foundation for building akka-based CQRS/DDD oriented applications.
+
+## License
+
+Licensed under the MIT License. See the LICENSE file for details.
---
+++ project/Projects.scala
@@ -0,0 +1,51 @@
+import sbt._
+import sbt.Keys._
+
+
+object Projects extends Build {
+  import play.twirl.sbt.SbtTwirl
+  import Settings._
+  import Unidoc.{settings => unidocSettings}
+  import Assembly.{settings => assemblySettings}
+  import Package.{serverSettings => packageServerSettings}
+  import Release.{settings => releaseSettings}
+  import Docker.{settings => dockerSettings}
+  import AspectJ.{settings => aspectJSettings}
+  import ScalaJs.{settings => scalaJsSettings}
+  import Dependencies._
+
+  lazy val root = Project(id = Globals.name, base = file("."))
+    .settings(basicSettings: _*)
+    .settings(noPublishing: _*)
+    .aggregate(
+      `akka-ddd-core`,
+      `akka-ddd-examples`
+    )
+
+  lazy val `akka-ddd-core` = module("core", basicSettings)
+    .settings(unidocSettings: _*)
+    .settings(assemblySettings: _*)
+    .settings(
+      libraryDependencies ++=
+        compile(typesafeConfig, akkaActor, akkaContrib, akkaPersistence, akkaSlf4j) ++
+        test(scalaTest, scalaCheck, scalaMock, akkaTest)
+    )
+
+  lazy val `akka-ddd-examples` = module("examples", basicSettings)
+    .settings(noPublishing: _*)
+    .settings(
+      libraryDependencies ++=
+        compile(typesafeConfig, logback, akkaActor, akkaContrib, akkaPersistence, akkaSlf4j) ++
+        test(scalaTest, scalaCheck, scalaMock, akkaTest)
+    ).dependsOn(
+      `akka-ddd-core` % "test->test;compile->compile"
+    )
+
+  def module(name: String, basicSettings: Seq[Setting[_]]): Project = {
+    val id = s"${Globals.name}-$name"
+    Project(id = id, base = file(id), settings = basicSettings ++ Seq(Keys.name := id))
+  }
+
+  val noPublishing: Seq[Setting[_]] = Seq(publish := { }, publishLocal := { }, publishArtifact := false)
+
+}
