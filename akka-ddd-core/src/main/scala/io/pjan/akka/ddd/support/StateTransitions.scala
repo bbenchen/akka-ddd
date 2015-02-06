226ba348a43942613b4d2aa41ea922cdf3745093
pjan vandaele <pjan.vandaele@gmail.com>
2015-02-06 12:08:51 +0900

    StateTransitions

---
+++ akka-ddd-core/src/main/scala/io/pjan/akka/ddd/support/StateTransitions.scala
@@ -0,0 +1,25 @@
+package io.pjan.akka.ddd.support
+
+import io.pjan.akka.ddd.EntityId
+import io.pjan.akka.ddd.event.Event
+
+object StateTransitions {
+  /**
+   * Type alias representing a HandleEvent-expression for an AggregateState.
+   */
+  type Transition[Id <: EntityId] = PartialFunction[Event, CommandHandler.HandleCommand[Id]]
+
+  /**
+   * emptyBehavior is a Evolution-expression that matches no events at all, ever.
+   */
+  def emptyBehavior[Id <: EntityId]: Transition[Id] = new Transition[Id] {
+    override def isDefinedAt(c: Event): Boolean = false
+    override def apply(c: Event): CommandHandler.HandleCommand[Id] = throw new UnsupportedOperationException("Empty behavior apply()")
+  }
+}
+
+trait StateTransitions[Id <: EntityId] {
+  import StateTransitions._
+
+  def transition: Transition[Id]
+}
